25d8bd653ad007284c391b3b650c8092e09fcb1a
Skylet8 <deni352002@gmail.com>
2023-11-12 02:10:45 +0300

    Добавлен интерфейс IDelivery и реализующий его метод класс PostDelivery для доставки по почте

---
+++ TestTask/IService/IDelivery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTask.Domain;
+
+namespace TestTask.IService
+{
+    public interface IDelivery
+    {
+        void AddDelivery(Delivery delivery);
+    }
+}
---
+++ TestTask/Service/PostDelivery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTask.Domain.Context;
+using TestTask.Domain;
+using TestTask.IService;
+
+namespace TestTask.Service
+{
+    internal class PostDelivery : IDelivery
+    {
+        public void AddDelivery(Delivery delivery)
+        {
+            using (DeliveriesContext db = new())
+            {
+                db.Deliveries.Add(delivery);
+                db.SaveChanges();
+            }
+        }
+    }
+}
