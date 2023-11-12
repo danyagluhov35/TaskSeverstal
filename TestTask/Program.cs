cb9465b733ff32950250b9eb137807577e1d1b60
Skylet8 <deni352002@gmail.com>
2023-11-12 03:38:27 +0300

    Был добалвен интерфейс ISimpleCostCalculation для рассчета стоимости продукта за киллограм. А так же реализующий его метод класс SimpleCostCalculation

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -1,9 +1,2 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using TestTask.Domain;
-using TestTask.Domain.Context;
-using TestTask.IService;
-using TestTask.Model;
-using TestTask.Service;
-
-
 
---
+++ TestTask/Service/SimpleCostCalculation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTask.IService;
+
+namespace TestTask.Service
+{
+    internal class SimpleCostCalculation : ISimpleCostCalculation
+    {
+        public decimal CalculateCost(int? weight, decimal? price)
+        {
+            if (weight == 0 || weight < 0)
+                throw new ArgumentException($"Вес не может быть равным 0 или отрицательным");
+            if (price == null)
+                throw new ArgumentException("Цена не определена");
+
+            return (decimal)(price / weight)!;
+        }
+    }
+}
